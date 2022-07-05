c75d822f5288edaf91774a5437e96af2c4787236
y9yk <andrew.yk82@gmail.com>
2022-07-05 14:54:17 +0900

    update ts files

--- dist/gcloud-stroage-file.interceptor.d.ts
+++ dist/gcloud-stroage-file.interceptor.d.ts
@@ -1,8 +1,4 @@
 import { NestInterceptor, Type } from '@nestjs/common';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { GCloudStoragePerRequestOptions } from './gcloud-storage.interface';
-export declare function GCloudStorageFileInterceptor(
-  fieldName: string,
-  localOptions?: MulterOptions,
-  gcloudStorageOptions?: Partial<GCloudStoragePerRequestOptions>,
-): Type<NestInterceptor>;
+export declare function GCloudStorageFileInterceptor(fieldName: string, localOptions?: MulterOptions, gcloudStorageOptions?: Partial<GCloudStoragePerRequestOptions>): Type<NestInterceptor>;
