cb8ae6c00072850f26a9608ed060948bd8ef7ae5
y9yk <andrew.yk82@gmail.com>
2022-08-17 20:55:12 +0900

    add ReadableBufferStream class

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { Readable } from 'stream';
+
 /**
  * https://gist.github.com/jed/982883
  * @param a
@@ -7,3 +9,21 @@ export const uuid = (a: string = ''): string =>
     ? /* eslint-disable no-bitwise */
       ((Number(a) ^ (Math.random() * 16)) >> (Number(a) / 4)).toString(16)
     : `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, uuid);
+
+// buffer to readable-stream
+export class ReadableBufferStream extends Readable {
+  private sent = false;
+
+  constructor(private buffer: any) {
+    super();
+  }
+
+  _read() {
+    if (!this.sent) {
+      this.push(this.buffer);
+      this.sent = true;
+    } else {
+      this.push(null);
+    }
+  }
+}
