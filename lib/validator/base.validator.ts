d3b87ed2ccadf508699fe5d69f798f20a254947b
y9yk <andrew.yk82@gmail.com>
2022-08-26 14:05:34 +0900

    update assertion rule
    - change created_date, last_updated_date to required cols.

--- lib/validator/base.validator.ts
+++ lib/validator/base.validator.ts
@@ -5,13 +5,14 @@ import { isNotEmpty, isValidDate, isValidJson } from '../common/utils';
 export const assertCateRow = (row: any, isJson: boolean) => {
   const { category_code, category_name, created_date, last_updated_date } = getCateCols(row, isJson);
   assert(
-    isNotEmpty(category_code) && isNotEmpty(category_name) && isNotEmpty(created_date),
+    isNotEmpty(category_code) && isNotEmpty(category_name) && isNotEmpty(created_date) && isNotEmpty(last_updated_date),
     getErrorMsg(ERROR_MESSAGE.NOT_FOUND_REQUIRED_COL_ERROR, row),
   );
   assert(isValidDate(created_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  if (last_updated_date && last_updated_date.length > 0) {
-    assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  }
+  assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // if (last_updated_date && last_updated_date.length > 0) {
+  //   assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // }
 };
 
 export const assertItemRow = (row: any, isJson: boolean) => {
@@ -20,16 +21,22 @@ export const assertItemRow = (row: any, isJson: boolean) => {
     isJson,
   );
   assert(
-    isNotEmpty(item_code) && isNotEmpty(category_code) && isNotEmpty(title) && isNotEmpty(image_url),
+    isNotEmpty(item_code) &&
+      isNotEmpty(category_code) &&
+      isNotEmpty(title) &&
+      isNotEmpty(image_url) &&
+      isNotEmpty(created_date) &&
+      isNotEmpty(last_updated_date),
     getErrorMsg(ERROR_MESSAGE.NOT_FOUND_REQUIRED_COL_ERROR, row),
   );
   if (isNotEmpty(is_deleted)) {
     assert(['Y', 'N'].includes(is_deleted), getErrorMsg(ERROR_MESSAGE.IS_DELETED_FORMAT_ERROR, row));
   }
   assert(isValidDate(created_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  if (last_updated_date && last_updated_date.length > 0) {
-    assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  }
+  assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // if (last_updated_date && last_updated_date.length > 0) {
+  //   assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // }
 };
 
 export const assertRevwRow = (row: any, isJson: boolean) => {
@@ -38,7 +45,11 @@ export const assertRevwRow = (row: any, isJson: boolean) => {
     isJson,
   );
   assert(
-    isNotEmpty(item_code) && isNotEmpty(review_code) && isNotEmpty(review) && isNotEmpty(created_date),
+    isNotEmpty(item_code) &&
+      isNotEmpty(review_code) &&
+      isNotEmpty(review) &&
+      isNotEmpty(created_date) &&
+      isNotEmpty(last_updated_date),
     getErrorMsg(ERROR_MESSAGE.NOT_FOUND_REQUIRED_COL_ERROR, row),
   );
   if (isNotEmpty(is_deleted)) {
@@ -48,9 +59,10 @@ export const assertRevwRow = (row: any, isJson: boolean) => {
     assert(isValidJson(filter_and_sort), getErrorMsg(ERROR_MESSAGE.FILTER_AND_SORT_FORMAT_ERROR, row));
   }
   assert(isValidDate(created_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  if (last_updated_date && last_updated_date.length > 0) {
-    assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
-  }
+  assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // if (last_updated_date && last_updated_date.length > 0) {
+  //   assert(isValidDate(last_updated_date), getErrorMsg(ERROR_MESSAGE.DATE_FORMAT_ERROR, row));
+  // }
 };
 
 const getCateCols = (row: any, isJson = false): any => {
